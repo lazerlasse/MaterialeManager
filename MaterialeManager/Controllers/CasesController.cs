15fadba9dd8c0e623e0de91131cf05612468a745
Lasse Antonsen <31047935+lazerlasse@users.noreply.github.com>
2020-03-27 01:07:52 +0100

    Added accept view and added accept get method in case controller. Functions for links on accept view not implemented in controller.

--- MaterialeManager/Controllers/CasesController.cs
+++ MaterialeManager/Controllers/CasesController.cs
@@ -10,177 +10,213 @@ using MaterialeManager.Models;
 using MaterialeManager.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using MaterialeManager.Authorization;
 
 namespace MaterialeManager
 {
-    [Authorize]
-    public class CasesController : CaseControllerBaseModel
-    {
-        public CasesController(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<IdentityUser> userManager)
-            :base(context, authorizationService, userManager)
-        {
-        }
+	[Authorize]
+	public class CasesController : CaseControllerBaseModel
+	{
+		public CasesController(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<IdentityUser> userManager)
+			: base(context, authorizationService, userManager)
+		{
+		}
 
-        // GET: Cases
-        public async Task<IActionResult> Index()
-        {
-            var cases = Context.Case
-                .Include(s => s.CaseState)
-                .Include(p => p.Photographer)
-                .Include(o => o.CaseOperator)
-                .AsNoTracking();
+		// GET: Cases
+		public async Task<IActionResult> Index()
+		{
+			var cases = Context.Case
+				.Include(s => s.CaseState)
+				.Include(p => p.Photographer)
+				.Include(o => o.CaseOperator)
+				.AsNoTracking();
 
-            return View(await cases.ToListAsync());
-        }
+			return View(await cases.ToListAsync());
+		}
 
-        // GET: Cases/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Cases/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var caseToView = await Context.Case
-                .Include(s => s.CaseState)
-                .AsNoTracking()
-                .FirstOrDefaultAsync(c => c.CaseID == id);
+			var caseToView = await Context.Case
+				.Include(s => s.CaseState)
+				.Include(o => o.CaseOperator)
+				.Include(p => p.Photographer)
+				.AsNoTracking()
+				.FirstOrDefaultAsync(c => c.CaseID == id);
 
-            if (caseToView == null)
-            {
-                return NotFound();
-            }
+			if (caseToView == null)
+			{
+				return NotFound();
+			}
 
-            return View(caseToView);
-        }
+			return View(caseToView);
+		}
 
-        // GET: Cases/Create
-        public async Task<IActionResult> Create()
-        {
-            var newCase = new Case
-            {
-                Created = DateTime.Now
-            };
+		// GET: Cases/Create
+		public IActionResult Create()
+		{
+			var newCase = new Case
+			{
+				Created = DateTime.Now
+			};
 
-            ViewBag.CaseStates = await PopulateCaseStatesDropDownList();
-            
-            return View(newCase);
-        }
+			return View(newCase);
+		}
 
-        // POST: Cases/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CaseID,Titel,Description,Comments,Created,CaseStateID")] Case caseToCreate)
-        {
-            if (ModelState.IsValid)
-            {
-                caseToCreate.PhotographerID = UserManager.GetUserId(User);
-                Context.Add(caseToCreate);
-                await Context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
+		// POST: Cases/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("CaseID,Titel,Description,Comments,Created")] Case caseToCreate)
+		{
+			if (ModelState.IsValid)
+			{
+				caseToCreate.PhotographerID = UserManager.GetUserId(User);
+				caseToCreate.CaseState = Context.CaseStates.FirstOrDefault(s => s.State == "Oprettet");
+				Context.Add(caseToCreate);
+				await Context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
 
-            ViewBag.CaseStates = await PopulateCaseStatesDropDownList();
+			return View(caseToCreate);
+		}
 
-            return View(caseToCreate);
-        }
+		// GET: Cases/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Cases/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			var caseToEdit = await Context.Case.FirstOrDefaultAsync(i => i.CaseID == id);
 
-            var caseToEdit = await Context.Case.FirstOrDefaultAsync(i => i.CaseID == id);
+			if (caseToEdit == null)
+			{
+				return NotFound();
+			}
 
-            if (caseToEdit == null)
-            {
-                return NotFound();
-            }
+			ViewBag.CaseStates = await PopulateCaseStatesDropDownList();
 
-            ViewBag.CaseStates = await PopulateCaseStatesDropDownList();
+			return View(caseToEdit);
+		}
 
-            return View(caseToEdit);
-        }
+		// POST: Cases/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("CaseID,Titel,Description,Comments,Created,CaseStateID")] Case caseToUpdate)
+		{
+			if (id != caseToUpdate.CaseID)
+			{
+				return NotFound();
+			}
 
-        // POST: Cases/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CaseID,Titel,Description,Comments,Created,CaseStateID")] Case caseToUpdate)
-        {
-            if (id != caseToUpdate.CaseID)
-            {
-                return NotFound();
-            }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					Context.Update(caseToUpdate);
+					await Context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!CaseExists(caseToUpdate.CaseID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    Context.Update(caseToUpdate);
-                    await Context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CaseExists(caseToUpdate.CaseID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+				return RedirectToAction(nameof(Index));
+			}
 
-                return RedirectToAction(nameof(Index));
-            }
+			ViewBag.CaseStates = await PopulateCaseStatesDropDownList();
 
-            ViewBag.CaseStates = await PopulateCaseStatesDropDownList();
+			return View(caseToUpdate);
+		}
 
-            return View(caseToUpdate);
-        }
+		// GET: Cases/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Cases/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			var caseToDelete = await Context.Case
+				.FirstOrDefaultAsync(m => m.CaseID == id);
 
-            var caseToDelete = await Context.Case
-                .FirstOrDefaultAsync(m => m.CaseID == id);
+			if (caseToDelete == null)
+			{
+				return NotFound();
+			}
 
-            if (caseToDelete == null)
-            {
-                return NotFound();
-            }
+			return View(caseToDelete);
+		}
 
-            return View(caseToDelete);
-        }
+		// POST: Cases/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var caseToDelete = await Context.Case
+				.FirstOrDefaultAsync(i => i.CaseID == id);
 
-        // POST: Cases/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var caseToDelete = await Context.Case
-                .FirstOrDefaultAsync(i => i.CaseID == id);
+			Context.Case.Remove(caseToDelete);
+			await Context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-            Context.Case.Remove(caseToDelete);
-            await Context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
 
-        private bool CaseExists(int id)
-        {
-            return Context.Case.Any(e => e.CaseID == id);
-        }
-    }
+		[HttpGet]
+		public async Task<IActionResult> Accept(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var caseToAccept = await Context.Case
+				.Include(p => p.Photographer)
+				.Include(s => s.CaseState)
+				.FirstOrDefaultAsync(c => c.CaseID == id);
+
+			if (caseToAccept == null)
+			{
+				return NotFound();
+			}
+
+			caseToAccept.CaseState = Context.CaseStates.FirstOrDefault(s => s.State == "Klippes");
+			caseToAccept.CaseOperatorID = UserManager.GetUserId(User);
+
+			try
+			{
+				Context.Case.Update(caseToAccept);
+				await Context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				throw;
+			}
+
+
+			return View(caseToAccept);
+		}
+
+		private bool CaseExists(int id)
+		{
+			return Context.Case.Any(e => e.CaseID == id);
+		}
+	}
 }
